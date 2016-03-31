000344e54ea3f6f082cc922d458f0fb37177429e
tgregory <trifonov_gm@nrcki.ru>
2016-03-31 08:46:31 +0300

    Simple watcher.

--- viirs-watcher.go
+++ viirs-watcher.go
@@ -3,162 +3,190 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"github.com/fsnotify/fsnotify"
+	"log"
 	"os"
-	"regexp"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 var (
-	ErrBadRegexp             = errors.New("The pattern regexp is malformed and fails to compile.")
-	ErrNoGroup               = errors.New("Regular expression does not provide the required capture group.")
-	ErrEmptyId               = errors.New("Failed to extract id from string.")
-	ErrDuplicateType         = errors.New("Duplicate file types are forbidden.")
-	ErrUnexpectedType        = errors.New("Received notification of unexpected type.")
-	ErrDuplicateNotification = errors.New("Data file of the same name created twice.")
+	IdMissmatch    = errors.New("Merging of states with different Ids is not allowed.")
+	UnexpectedName = errors.New("Filename does not satisfy the expected pattern.")
+	DetectFailed   = errors.New("Detect binary launch failed.")
+	FitFailed      = errors.New("Fit binary launch failed.")
 )
 
-const idName = "id"
-
-type Pattern struct {
-	Pattern  *regexp.Regexp
-	subIndex int
-}
-
-/* json.Marshaller interface */
-func (p *Pattern) MarshalJSON() ([]byte, error) {
-	s := ""
-	if nil != p && nil != p.Pattern {
-		s = p.Pattern.String()
-	}
-	return json.Marshal(s)
-}
-
-/* json.Unmarshaller interface */
-func (p *Pattern) UnmarshalJSON(bytes []byte) error {
-	var s string
-	err := json.Unmarshal(bytes, &s)
-	if nil != err {
-		return err
-	}
-	p.Pattern, err = regexp.Compile(s)
-	if nil != err {
-		return ErrBadRegexp
-	}
-	for i, sub := range p.Pattern.SubexpNames() {
-		if sub == idName {
-			p.subIndex = i
-			return nil
-		}
-	}
-	return ErrNoGroup
-}
-
-func (p *Pattern) GetId(s string) (string, error) {
-	id := ""
-	matches := p.Pattern.FindAllStringSubmatch(s, -1)
-	for _, match := range matches {
-		id += match[p.subIndex]
-	}
-	if "" == id {
-		return "", ErrEmptyId
-	}
-	return id, nil
-}
-
-type DataFile struct {
-	Name    string
-	Path    string
-	Pattern Pattern
-}
-
-func (df *DataFile) UniqueName() string {
-	s := df.Name
-	if "" == s {
-		s = df.Pattern.Pattern.String()
-	}
-	return s
-}
+var (
+	//bands = []string{"DNB", "SVM07", "SVM08", "SVM10", "SVM12", "SVM13", "SVM14", "SVM15", "SVM16"}
+	version       = "v2.1"
+	defaultPath   = "."
+	defaultDetect = "vnf_detect"
+	defaultFit    = "vnf_fit"
+	m10           = "SVM10"
+	gmtco         = "GMTCO"
+	iicmo         = "IICMO"
+)
 
 type Config struct {
-	InputPath    string
-	OutputPath   string
+	WatchDir     string
+	OutputDir    string
 	DetectBinary string
 	FitBinary    string
 	ReduceBinary string
-	Env          map[string]string
-	Files        []DataFile
+	RequireBands []string
+	RequireGMTCO bool
+	RequireIICMO bool
 }
 
-type Data struct {
-	Id       string
-	Type     string
-	Filename string
-	Filepath string
-}
-
-type Pack struct {
-	cfg      *Config
-	received int
-	expected int
-	index    map[string]bool
-	Data     []Data
-}
-
-func NewPack(cfg Config) (*Pack, error) {
-	var p Pack
-	p.cfg = &cfg
-	p.index = make(map[string]bool)
-	for _, df := range cfg.Files {
-		name := df.UniqueName()
-		_, ok := p.index[name]
-		if ok {
-			return nil, ErrDuplicateType
+func (c *Config) Constrain() {
+	for _, s := range c.RequireBands {
+		if m10 == s {
+			return
 		}
-		p.index[name] = false
 	}
-	p.expected = len(p.index)
-	return &p, nil
+	c.RequireBands = append(c.RequireBands, m10)
+	if "" == c.OutputDir {
+		c.OutputDir = defaultPath
+	}
+	if "" == c.WatchDir {
+		c.WatchDir = defaultPath
+	}
+	if "" == c.DetectBinary {
+		c.DetectBinary = defaultDetect
+	}
+	if "" == c.FitBinary {
+		c.FitBinary = defaultFit
+	}
 }
 
-func (p *Pack) Full() bool {
-	return p.expected == p.received
+func (c *Config) Done(s *State) bool {
+	if c.RequireGMTCO != s.GMTCO {
+		return false
+	}
+	if c.RequireIICMO != s.IICMO {
+		return false
+	}
+	index := make(map[string]struct{})
+	for _, b := range s.Bands {
+		index[b] = struct{}{}
+	}
+	for _, b := range c.RequireBands {
+		if _, ok := index[b]; !ok {
+			return false
+		}
+	}
+	return true
 }
 
-func (p *Pack) Notify(d Data) error {
-	v, ok := p.index[d.Type]
-	if !ok {
-		return ErrUnexpectedType
+func (c *Config) Process(s *State) error {
+	detfile := filepath.Join(c.OutputDir, strings.Join([]string{"VNFD", s.Id, version}, "_")) + ".csv"
+	detect := exec.Command(c.DetectBinary, s.M10File, "-output", detfile, "-cloud", "0")
+	err := detect.Run()
+	if nil != err {
+		return DetectFailed
 	}
-	if v {
-		return ErrDuplicateNotification
+	fitfile := filepath.Join(c.OutputDir, strings.Join([]string{"VNFL", s.Id, version}, "_")) + ".csv"
+	fit := exec.Command(c.FitBinary, detfile, "-output", fitfile, "-plot", "1", "-map", "1", "-localmax", "1", "-size", "100", "-font", "10")
+	err = fit.Run()
+	if nil != err {
+		return FitFailed
 	}
-	p.index[d.Type] = true
-	p.Data = append(p.Data, d)
 	return nil
 }
 
-func (p *Pack) Process() error {
-
+type State struct {
+	Id      string
+	M10File string
+	Bands   []string
+	GMTCO   bool
+	IICMO   bool
 }
 
-func work(cfg Config, data <-chan Data) {
-	var err error
-	packs := make(map[string]*Pack)
-	for d := range data {
-		p, ok := packs[d.Id]
-		if !ok {
-			p, err = NewPack(cfg.Files)
-			if nil != err {
-				// !TODO log
-			}
-			packs[d.Id] = p
-		}
-		p.Notify(d)
-		if !p.Full() {
+func NewState(fp string) (*State, error) {
+	var s State
+	name := filepath.Base(fp)
+	parts := strings.Split(name, "_")
+	if len(parts) < 6 {
+		return nil, UnexpectedName
+	}
+	s.Id = strings.Join(parts[1:5], "_")
+	if strings.HasPrefix(parts[0], "SV") {
+		s.Bands = append(s.Bands, parts[0])
+	}
+	if parts[0] == m10 {
+		s.M10File = fp
+	}
+	return &s, nil
+}
+
+func (s *State) Merge(s2 *State) error {
+	if s.Id != s2.Id {
+		return IdMissmatch
+	}
+	s.Bands = append(s.Bands, s2.Bands...)
+	if s.M10File == "" {
+		s.M10File = s2.M10File
+	}
+	s.GMTCO = s.GMTCO || s2.GMTCO
+	s.IICMO = s.IICMO || s2.IICMO
+	return nil
+}
+
+func work(cfg Config, files <-chan string) {
+	awaiting := make(map[string]*State)
+	for f := range files {
+		s, err := NewState(f)
+		if nil != err {
+			log.Printf("State creation fail: %s for %s\n", err.Error(), f)
 			continue
 		}
-
+		if s2, ok := awaiting[s.Id]; ok {
+			s.Merge(s2)
+		}
+		awaiting[s.Id] = s
+		if !cfg.Done(s) {
+			continue
+		}
+		delete(awaiting, s.Id)
+		if err = cfg.Process(s); nil != err {
+			log.Printf("Processing failed: %s for %s\n", err.Error(), s.Id)
+		}
 	}
+	log.Printf("Notifications channel closed. All done. \n")
 }
 
 func main() {
+	var cfg Config
+	if len(os.Args) >= 2 {
+		fcfg, err := os.Open(os.Args[1])
+		if nil != err {
+			log.Panicln("Failed to open config.")
+		}
+		dec := json.NewDecoder(fcfg)
+		if err = dec.Decode(&cfg); nil != err {
+			log.Panicln("Failed to parse config.")
+		}
+	}
+	cfg.Constrain()
+	notifications := make(chan string)
+	watcher, err := fsnotify.NewWatcher()
+	if nil != err {
+		log.Panicf("Failed to start watcher: %s\n", err.Error())
+	}
+	if err = watcher.Add(cfg.WatchDir); nil != err {
+		log.Panicf("Failed to start watching directory: %s\n", err.Error())
+	}
+	go work(cfg, notifications)
+	for {
+		select {
+		case event := <-watcher.Events:
+			if event.Op&fsnotify.Write == fsnotify.Write && event.Name != cfg.WatchDir {
+				notifications <- event.Name
+			}
+		case e := <-watcher.Errors:
+			log.Printf("Watcher error: %s", e.Error())
+		}
+	}
 }
